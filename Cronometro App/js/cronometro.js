504cb46c7853269cc47cf4cbf358256ee5eb9f63
juninhoall <luiz.almeida@catolicasc.org.br>
2016-12-20 17:52:41 -0200

    Commit da aplicação

---
+++ Cronometro App/js/cronometro.js
@@ -0,0 +1,21 @@
+(function($) {
+AddTableRow = function() {
+var nomeRobo = $("#nomeRobo").val();
+var Horas= document.getElementById("Horas").innerHTML
+var Minutos= document.getElementById("Minutos").innerHTML
+var Segundos= document.getElementById("Segundos").innerHTML
+var Centesimas = document.getElementById("Centesimas").innerHTML
+
+
+console.log(nomeRobo);
+var newRow = $("<tr>");
+var cols = "";
+
+cols += '<td>'+nomeRobo+'</td>';
+cols += '<td>'+Horas+''+Minutos+''+Segundos+''+Centesimas+'</td>';
+
+newRow.append(cols);
+$("#products-table").append(newRow);
+return false;
+};
+})(jQuery);
---
+++ Cronometro App/js/ordenar.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('#products-table').DataTable();
+} );
