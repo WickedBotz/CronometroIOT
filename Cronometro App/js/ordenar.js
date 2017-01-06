12648d4374f366dc798aec4a71f00bf310b7cd5d
igorfachini <igor.fachini@catolicasc.org.br>
2017-01-06 19:32:13 -0200

    adicionado sorts para organizar tabela

--- Cronometro App/js/ordenar.js
+++ Cronometro App/js/ordenar.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-    $('#products-table').DataTable();
-} );
+// $(document).ready(function() {
+//     $('#products-table').DataTable();
+// } );
+//
+// function ordenarTabela() {
+//   var ativado = true;
+//   var sort;
+//   if(ativado){
+//    sort = new Tablesort(document.getElementById('products-table'));
+//    ativado = false;
+//   }
+//   // Make some Ajax request to fetch new data and on success:
+//   sort.refresh();
+//   console.log("ordenei");
+// }
