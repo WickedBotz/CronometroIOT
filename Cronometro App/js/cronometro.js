8eff39d6a3c77d937d2f480309ae8bcd000336eb
Luiz Almeida Júnior <allmeidajr@gmail.com>
2017-01-14 21:07:08 -0200

    Persistencia
    Tentando persistir no firebase

--- Cronometro App/js/cronometro.js
+++ Cronometro App/js/cronometro.js
@@ -1,11 +1,16 @@
 (function($) {
 AddTableRow = function() {
+var fire = new Firebase(https://cronometroiot.firebaseio.com/analise/");
 var nomeRobo = $("#nomeRobo").val();
 var Horas= document.getElementById("Horas").innerHTML
 var Minutos= document.getElementById("Minutos").innerHTML
 var Segundos= document.getElementById("Segundos").innerHTML
 var Centesimas = document.getElementById("Centesimas").innerHTML
 
+fire.set({
+usuario : nomeRobo,
+city : Horas
+});
 
 console.log(nomeRobo);
 var newRow = $("<tr>");
