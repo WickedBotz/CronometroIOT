b6aafb86689648a6ba4396644f6b49943f41e9b2
Luiz Almeida Júnior <allmeidajr@gmail.com>
2017-01-25 02:05:29 -0200

    dinamico

--- Cronometro App/js/cronometro.js
+++ Cronometro App/js/cronometro.js
@@ -1,26 +1,22 @@
+var ref = new Firebase("https://cronometroiot.firebaseio.com/analises");
+
+
 (function($) {
-AddTableRow = function() {
-//var fire = new Firebase(https://cronometroiot.firebaseio.com/analise/");
-var nomeRobo = $("#nomeRobo").val();
-var Horas= document.getElementById("Horas").innerHTML
-var Minutos= document.getElementById("Minutos").innerHTML
-var Segundos= document.getElementById("Segundos").innerHTML
-var Centesimas = document.getElementById("Centesimas").innerHTML
-
-//fire.set({
-//usuario : nomeRobo,
-//city : Horas
-//});
-
-console.log(nomeRobo);
-var newRow = $("<tr>");
-var cols = "";
-
-cols += '<td>'+nomeRobo+'</td>';
-cols += '<td>'+Horas+''+Minutos+''+Segundos+''+Centesimas+'</td>';
-
-newRow.append(cols);
-$("#products-table").append(newRow);
-return false;
+window.onload = function AddTableRow() {
+  ref.once("value", function(snapshot) {
+    var count = 0;
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val();
+      var newRow = $("<tr>");
+      var cols = "";
+      count++;
+      cols += '<td>'+count+'</td>';
+      cols += '<td>'+childData.nome+'</td>';
+      cols += '<td>'+childData.horas+':'+childData.minutos+':'+childData.segundos+':'+childData.centesimas+'</td>';
+      newRow.append(cols);
+      $("#products-table").append(newRow);
+      return false;
+    });
+  });
 };
 })(jQuery);
